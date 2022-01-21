f8422bb93766fbc0bb3c4f1ef21e3a11f459113e
Rashit Gareyev <rashit.gareev@akvelon.com>
2022-01-21 14:21:50 +0300

    topic creation added

--- src/main/java/com/pet/project/carsharingapp/CarSharingAppApplication.java
+++ src/main/java/com/pet/project/carsharingapp/CarSharingAppApplication.java
@@ -1,13 +1,31 @@
 package com.pet.project.carsharingapp;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class CarSharingAppApplication {
 
+    @Value("${car-sharing-app.kafka.topic-name}")
+    private String topicName;
+
     public static void main(String[] args) {
         SpringApplication.run(CarSharingAppApplication.class, args);
+    }
+
+    @Bean
+    NewTopic createCarRequestTopic(){
+        return TopicBuilder
+                .name(topicName)
+                .partitions(1)
+                //TODO: если ставить больше 1 реплики, то топик не создается.
+                // Видимо потому что у нас создан только один бутстрап сервер
+                .replicas(1)
+                .build();
     }
 
 }
