59048b2da5049baa2a1df8771b429478307092c0
Rashit Gareyev <rashit.gareev@akvelon.com>
2022-02-04 12:22:37 +0300

    swagger annotations added

--- src/main/java/com/pet/project/carsharingapp/controller/api/CarSharingService.java
+++ src/main/java/com/pet/project/carsharingapp/controller/api/CarSharingService.java
@@ -2,15 +2,20 @@ package com.pet.project.carsharingapp.controller.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.pet.project.carsharingapp.request.CarRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
+@Api(value = "Car sharing service")
 @RestController
 public interface CarSharingService {
 
+    @ApiOperation(value = "Reserve car or cancel reservation")
     @PostMapping(value = "/rent")
-    void rent(@Valid CarRequest request) throws JsonProcessingException;
+    void rent(@Valid @NotNull CarRequest request) throws JsonProcessingException;
 
 }
--- src/main/java/com/pet/project/carsharingapp/request/CarRequest.java
+++ src/main/java/com/pet/project/carsharingapp/request/CarRequest.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-@ApiModel
+@ApiModel("CarRequest")
 public class CarRequest implements Serializable {
 
     @NotNull
